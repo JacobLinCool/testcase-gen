41650eefeea537db9f920b1d74940227fb67c2d7
JacobLinCool <jacoblincool@gmail.com>
2021-10-29 19:27:35 +0800

    refactor(core): delete default export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Generator from "./Generator";
 import { recipe, testcase, generateMode, testcaseGenerator } from "./types";
 
-export default Generator;
 export { Generator, recipe, testcase, generateMode, testcaseGenerator };
